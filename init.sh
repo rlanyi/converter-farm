fb36b9cd3218a8adce6e6c6960df010446b1357c
Robert Lanyi <robert.lanyi@gravityrd.com>
2017-03-30 00:31:41 +0200

    add cgroup to install list

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo A38 Converter Farm --- Starting deploy scripts
-sudo apt-get install unzip nano
+sudo apt-get install unzip nano libcgroup1
 curl -O https://releases.hashicorp.com/consul/0.7.5/consul_0.7.5_linux_amd64.zip
 curl -O https://releases.hashicorp.com/nomad/0.5.5/nomad_0.5.5_linux_amd64.zip
 sudo unzip consul_0.7.5_linux_amd64.zip -d /usr/local/bin
