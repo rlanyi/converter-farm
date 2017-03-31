a0084f14f8960feddb5f4922e60f9feea2a0281f
Robert Lanyi <robert.lanyi@gravityrd.com>
2017-04-01 01:42:38 +0200

    add cron and logrotate

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo apt-get -y install unzip nano mc net-tools telnet libcgroup1
+sudo apt-get -y install unzip nano mc net-tools telnet cron logrotate libcgroup1
+sudo mv /etc/cron.daily/logrotate /etc/cron.hourly/logrotate
+
 curl -O http://192.168.1.147/hashicorp/consul_0.7.5_linux_amd64.zip
 curl -O http://192.168.1.147/hashicorp/nomad_0.5.5_linux_amd64.zip
 sudo unzip consul_0.7.5_linux_amd64.zip -d /usr/local/bin
