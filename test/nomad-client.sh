b0f99179c4164fae54af6c846284f90d15e257cd
Robert Lanyi <robert.lanyi@gravityrd.com>
2017-04-01 02:02:38 +0200

    improve nomad-client startup script

--- test/nomad-client.sh
+++ test/nomad-client.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-sudo sh -c "nomad agent -config $DIR/nomad-client >>/var/log/messages 2>&1 &"
+sudo screen -dm bash -c "while true; do nomad agent -config $DIR/nomad-client >>/var/log/nomad-client.log 2>&1 ; sleep 10 ; done"
