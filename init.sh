edaba8f6616d197387abd9a77371122d5584cc82
Robert Lanyi <robert.lanyi@gravityrd.com>
2017-04-01 02:13:18 +0200

    add dashboard

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-sudo apt-get -y install unzip nano mc net-tools telnet cron logrotate screen libcgroup1
+sudo apt-get -y install unzip nano mc net-tools telnet cron logrotate screen npm libcgroup1
 sudo mv /etc/cron.daily/logrotate /etc/cron.hourly/logrotate
 
 curl -O http://192.168.1.147/hashicorp/consul_0.7.5_linux_amd64.zip
@@ -10,6 +10,15 @@ curl -O http://192.168.1.147/hashicorp/nomad_0.5.5_linux_amd64.zip
 sudo unzip consul_0.7.5_linux_amd64.zip -d /usr/local/bin
 sudo unzip nomad_0.5.5_linux_amd64.zip -d /usr/local/bin
 
+rm *.zip
 sudo apt-get clean
 
 $DIR/test/nomad-client.sh
+
+git clone https://github.com/yaronn/blessed-contrib.git
+cd blessed-contrib
+npm install
+cd $DIR
+node blessed-contrib/examples/dashboard.js
+
+reboot
